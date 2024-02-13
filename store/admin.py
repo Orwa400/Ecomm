3514852ebf80a0a27d9b62902753c06d7ff6b5cd
Orwa400 <matheworwa3@gmail.com>
2024-02-13 15:50:44 +0300

    Update admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Customer, Product, Order
 
-# Register your models here.
+# Register the  models.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+
