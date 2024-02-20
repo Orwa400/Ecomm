fb004d650f1469d0ee2e775925d6635318fd0fd0
Orwa400 <matheworwa3@gmail.com>
2024-02-20 10:51:42 +0300

    Add navbar page

--- store/models.py
+++ store/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 # Customers
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
@@ -27,6 +30,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Add Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
     def __str__(self):
         return self.name
