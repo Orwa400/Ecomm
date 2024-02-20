b47d3db7d9fe67897904aff008ea1699e7ba2644
Orwa400 <matheworwa3@gmail.com>
2024-02-20 14:26:02 +0300

    Add login and logout page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
     products = Product.objects.all
@@ -7,3 +10,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html',{})
+
+def login_user(request):
+    return render(request, 'login_html', {})
+
+def logout_user(request):
+    pass
