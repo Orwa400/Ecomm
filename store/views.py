3a877f724f6773888dbcc6b7c890df570dee50e9
Orwa400 <matheworwa3@gmail.com>
2024-03-12 13:04:25 +0300

    Update User Credentials

--- store/views.py
+++ store/views.py
@@ -4,9 +4,49 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
 
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Did they fill out the form
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            # Is the form valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your Password Has Been Updated...")
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+        
+        else:
+            form = ChangePasswordForm(current_user)
+    else:
+        messages.success(request, "You Must Be Logged In To View That!")
+        return redirect('home')
+
+    return render(request, "update_password.html", {'form':form})
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User Has Been Updated!")
+            return redirect('home')
+        return render(request, "update_user.html", {'user_form':user_form})
+        
+    else:
+        messages.success(request, "You Must be Logged In to Access That Page!!")
+        return redirect('home')
 
 def category_summary(request):
     categories = Category.objects.all()
