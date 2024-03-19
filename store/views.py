d4d40c62281301f38366c39e449a001229285df1
Orwa400 <matheworwa3@gmail.com>
2024-03-19 10:29:41 +0300

    Add Shipping Information form

--- store/views.py
+++ store/views.py
@@ -5,10 +5,16 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
 from django import forms
 from django.db.models import Q
 import json
 from cart.cart import Cart
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def search(request):
     # Deternmine if the user filled out the form
@@ -25,18 +31,37 @@ def search(request):
     else:
         return render(request, "search.html", {})
 
+
 def update_info(request):
     if request.user.is_authenticated:
+        # Get Current User
         current_user = Profile.objects.get(user__id=request.user.id)
-        form = UserInfoForm(request.POST or None, instance=current_user)
+        # Get Current User's Shipping Info
+        shipping_user = None
 
-        if form.is_valid():
+        try:
+            shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        except ObjectDoesNotExist:
+            pass
+
+        # Get original User Form
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        # Get User's Shipping Form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+
+        if form.is_valid() and shipping_form.is_valid():
+            # Save original form
             form.save()
-            messages.success(request, "Your Info Has Been Updated")
+            # Save shipping form
+            shipping_form.save()
+
+            messages.success(request, "Your Info Has Been Updated!!")
             return redirect('home')
-        return render(request, "update_info.html", {'form': form}) 
+        
+        # If either form is not valid, render the update_info page with the forms
+        return render(request, "update_info.html", {'form': form, 'shipping_form': shipping_form})
     else:
-        messages.success(request, "You Must Be Logged In To View That!!")
+        messages.success(request, "You Must Be Logged In To Access That Page!!")
         return redirect('home')
  
 
